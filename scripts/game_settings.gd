42f87f9f14ea88cd2690328f5b2461e7fa15387a
RobertFOConnor <robertoconnor94@gmail.com>
2025-09-28 15:37:25 +0300

    alt enter to fullscreen toggle

--- scripts/game_settings.gd
+++ scripts/game_settings.gd
@@ -22,3 +22,12 @@ func getSFXLevel():
 		return -80
 	else:
 		return linear_to_db(sfxLevel / 100.0)
+
+func _process(delta):
+	var window = get_window()
+	if Input.is_key_pressed(Key.KEY_ALT) && Input.is_action_just_pressed("submit"):
+		var current_mode = DisplayServer.window_get_mode()
+		if current_mode != DisplayServer.WindowMode.WINDOW_MODE_EXCLUSIVE_FULLSCREEN:
+			DisplayServer.window_set_mode(DisplayServer.WindowMode.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
+		else:
+			DisplayServer.window_set_mode(DisplayServer.WindowMode.WINDOW_MODE_WINDOWED)
